f434ef25a3af3b181143c6e7f27a757fa3c75b8b
Colin Rodrigues <colin.rods@gmail.com>
2024-09-27 15:26:29 +0200

    commit

--- scoreboard.py
+++ scoreboard.py
@@ -13,17 +13,23 @@ class Scoreboard(Turtle):
         self.color("white")
         self.hideturtle()
         self.score = 0
+        self.high_score = 0
         self.update_score()
 
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_score()
 
     def increase_score(self,):
-        self.clear()
         self.score += 1
         self.update_score()
 
 
     def update_score(self,):
-        self.write(f"Score: {self.score}", False, align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", False, align=ALIGNMENT, font=FONT)
 
     def game_over(self):
         self.goto(0,0)
--- snake.py
+++ snake.py
@@ -1,5 +1,7 @@
 from turtle import Turtle
 
+from main import segment
+
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 UP = 90
@@ -32,7 +34,12 @@ class Snake:
     def extend(self):
         self.add_segment(self.segments[-1].position())
 
-
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000,1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
 
     def move(self):
         for seg in range(len(self.segments) - 1, 0, -1):
